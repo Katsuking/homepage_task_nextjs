77745cfec00c4b4b5bafb3b589624de2cc06f1e9
kusaka <s.kusaka@oplan.co.jp>
2024-03-19 15:01:57 +0900

    Solutionを追加

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Solution from '@/components/body/Solution'
 import SuggestionPage from '@/components/body/Suggestion'
 import TopPage from '@/components/body/Top'
 
@@ -6,6 +7,7 @@ export default function Home() {
     <main>
       <TopPage />
       <SuggestionPage />
+      <Solution />
       <div className="py-[500px]"></div>
     </main>
   )
