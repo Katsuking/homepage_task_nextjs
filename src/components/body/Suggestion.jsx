83bb6521b51f95df2030f8a44c2cbbe5261f266d
Katsuking <legendsoccerforte5@gmail.com>
2024-03-21 10:43:03 +0900

    Suggestion: change btn color

--- src/components/body/Suggestion.jsx
+++ src/components/body/Suggestion.jsx
@@ -69,7 +69,7 @@ const SuggestionPage = () => {
           />
           <div>
             <p>
-              時間と場所を選ばず
+              時間と場所を選ばない場所で
               <span className="text-red-500">BCP対策もしっかりしたい</span>
             </p>
           </div>
