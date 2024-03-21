1a9ddf60f222d1f4d6be805176aa2586be736367
github2022Oug <walnutr20912@gmail.com>
2024-03-21 16:47:18 +0900

    update responsive

--- src/components/body/Price.jsx
+++ src/components/body/Price.jsx
@@ -1,16 +1,37 @@
 import Price_oplan from './Price_oplan'
 import Price_general from './Price_general'
+import Card from './Card'
 
 const Price = () => {
   return (
     <section>
       <div className="px-10  items-center text-center">
-        <h2 className="text-6xl font-black">料金プラン</h2>
-        {/* diff */}
-        <div className="diff aspect-[9/16] md:aspect-[16/9]">
+        <h2 className="text-4xl sm:text-5xl md:text-6xl font-black">
+          料金プラン
+        </h2>
+        {/* diffを使用した表示 */}
+        <div className="md:aspect-[16/11] lg:aspect-[16/9] hidden md:diff">
           <Price_oplan />
           <Price_general />
           <div className="diff-resizer"></div>
+        </div>
+        {/* Cardのみの表示。他社比較との料金比較は省略 */}
+        <div className="md:hidden">
+          <Card
+            title="sample"
+            description="sampleああｋｇｈｆｋｓｄｊｖｍｓｄｍｊｈｖｋｖｋｚｊｖｋｍｊｆｋｖｍｊｘ、ｖｊｌｘｚｊｖ、ｚｊｖくふぇｍふぉあぁｋｊｆｄあｋｋｊｄｆｋじゃｓｋｄｊｆぁｓｊｆｌｋｄｓｌｋｆｓｌｋｆｊｌｋｓだｊｋｆｄｓｋｆじゃｌｋｄｊｆｌｋｄｆｌｋｄじゃｆｌｋｄｊｋｆｊｌｋｄ"
+            imgurl="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+          />
+          <Card
+            title="sample"
+            description="ｖｋｍｊｆｋｖｍｊｘ、ｖｊｌｘｚｊｖ、ｚｊｖくふぇｍふぉあぁｋｊｆｄあｋ"
+            imgurl="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+          />
+          <Card
+            title="sample"
+            description="sample"
+            imgurl="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+          />
         </div>
       </div>
     </section>
