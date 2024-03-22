8a8714bcaa2a5187530562ac841ed95255d4c13b
github2022Oug <walnutr20912@gmail.com>
2024-03-22 16:23:32 +0900

    Price_general's design bag fixed

--- src/components/body/Price_general.jsx
+++ src/components/body/Price_general.jsx
@@ -10,28 +10,30 @@ const Price_general = () => {
           <div className="h-[15%] text-center items-center">
             <h3 className="pt-5 text-gray-200 text-5xl">GENERAL</h3>
           </div>
-          <div className="h-[85%] justify-center items-stretch grid grid-rows-1 grid-cols-3 grid-flow-row gap-4">
-            {/* 1 */}
-            <Card
-              title="Microsoft 365
+          <div className="h-[85%]">
+            <div className=" justify-center items-stretch grid grid-rows-1 grid-cols-3 grid-flow-row gap-4">
+              {/* 1 */}
+              <Card
+                title="Microsoft 365
               Business Basic"
-              price="1200"
-              imgurl="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            />
-            {/* 2 */}
-            <Card
-              title="Microsoft 365
+                price="1200"
+                imgurl="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              />
+              {/* 2 */}
+              <Card
+                title="Microsoft 365
               Business Standard"
-              price="2500"
-              imgurl="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            />
-            {/* 3 */}
-            <Card
-              title="Microsoft 365
+                price="2500"
+                imgurl="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              />
+              {/* 3 */}
+              <Card
+                title="Microsoft 365
               Business Premium"
-              price="3000"
-              imgurl="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            />
+                price="3000"
+                imgurl="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              />
+            </div>
           </div>
         </div>
       </div>
