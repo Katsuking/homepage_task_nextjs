dfba6cf0c2922440478d804fbdefe45367b7d6d7
Katsuking <legendsoccerforte5@gmail.com>
2024-03-26 15:37:38 +0900

    Top p tag

--- src/components/body/Top.jsx
+++ src/components/body/Top.jsx
@@ -8,16 +8,17 @@ const TopPage = () => {
         }}
       >
         <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-center text-neutral-content">
+        <div className="hero-content text-neutral-content">
           <div className="max-w-md">
-            <h1 className="mb-5 text-2xl font-bold">
-              <span className="">オープランで</span>
-              <br /> Microsoft365の導入はじめませんか?
+            <h1 className="mb-5 text-2xl font-bold text-start">
+              <span className="font-bold">オープランで</span>
+              <br />
+              <span className="sm:ml-8">Microsoft365の導入はじめませんか?</span>
             </h1>
             <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              クラウドベースのサービスであるため、どこからでもアクセスが可能です。
+              <br />
+              また、Word、Excel、PowerPointなど、おなじみのアプリケーションを最新の状態で利用でき、安全な環境での作業を保証します。
             </p>
             <button className="btn border-[#00004d] text-white bg-[#00004d]">
               無料相談してみる
